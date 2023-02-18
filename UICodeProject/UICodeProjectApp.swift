d45ebc6bf83cef2358eba86be7407693a846afe3
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2023-02-18 11:02:41 -0500

    Initial Commit

---
+++ UICodeProject/UICodeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UICodeProjectApp.swift
+//  UICodeProject
+//
+//  Created by Ishaan Bhasin on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UICodeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
